125fffbc5a226bd5f324c7222fa366753dab7fb1
jacekv <jaca347@gmail.com>
2020-07-03 16:51:43 +0200

    Working js bip39

--- bip39.js
+++ bip39.js
@@ -1,41 +1,79 @@
-let crypto = require("crypto");
-let fs = require("fs");
+const crypto = require("crypto");
+const wordList = require('./english.js');
 
-//32 bytes = 256 bits
-let size = 32;
-let sha256 = crypto.createHash('sha256');
 
-let content = fs.readFileSync('english.txt', 'utf8');
-let words = content.split("\n");
+//16 bytes = 128 bits
+let size = 128;
 
-let mnemonic = "";
+function hashData(data) {
+    let sha256 = crypto.createHash('sha256');
+    return sha256.update(data).digest();
+}
 
-crypto.randomBytes(size, (err, entropy) => {
-    if (err) throw err;
-    let hash = sha256.update(entropy).digest();
+function InvalidSizeException(message) {
+    this.message = message;
+    this.name = 'InvalidSizeException'
+}
 
+function mapWords(checksummed_entropy) {
+    let mnemonic = '';
+    for(let i = 0; i < checksummed_entropy.length; i += 11) {
+        value = parseInt(checksummed_entropy.slice(i, (i+11)), 2)
+        mnemonic += wordList.numberToWord[value] + ' '; 
+    }
+    return mnemonic;
+}
+
+function mapNumbers(mnemonic) {
+    mnemonic = mnemonic.split(' ');
+    // remove empty string
+    mnemonic.pop();
+    let binary = '';
+    // mapping from word to bi
+    for (const word of mnemonic) {
+        bin = wordList.wordToNumber[word].toString(2)
+        binary += "00000000000".substr(bin.length) + bin;
+    }
+    let checksumLength = Math.floor(binary.length / 32)
+    let checksum = binary.slice(binary.length - checksumLength);
+    let values = [];
+    //
+    for(let i = 0; i < binary.length - checksumLength; i += 8) {
+        value = parseInt(binary.slice(i, (i+8)), 2)
+        values.push(value)
+    }
+    let hash = hashData( new Buffer.from(values));
+    let hash_binary = hash[0].toString(2);
+    hash_binary = hash_binary.slice(0, checksumLength)
+    if (hash_binary === checksum) {
+        console.log('All good')
+    } else {
+        console.log('Invalid');
+    }
+}
+
+function gen_mnemonic(size) {
+    if (size < 128 || size > 256 || size % 32 !== 0) {
+        throw new InvalidSizeException('The provided size is invalid');
+    }
+    let entropy = crypto.randomBytes(size / 8);
     let binary;
     let entropy_binary = "";
-    let l;
+    // change from byte buffer to binary string
     for (const b of entropy) {
         binary = b.toString(2);
-        l = binary.length;
-        if(l < 8){
-            binary = "0".repeat(8 - l) + binary;
-        }
-        entropy_binary += binary;
+        entropy_binary += "00000000".substr(binary.length) + binary;
     }
+    let hash = hashData(entropy);
     let hash_binary = hash[0].toString(2);
-    let n = (size * 8) / 32;
-    hash_binary = hash_binary.slice(0, n);
-    l = hash_binary.length;
-    if(l < n) {
-        hash_binary = "0".repeat((n - l)) + hash_binary;
-    }
-    let checksummed_entropy_binary = entropy_binary + hash_binary.slice(0, n);
-    for(let i = 0; i < checksummed_entropy_binary.length; i += 11) {
-        value = parseInt(checksummed_entropy_binary.slice(i, (i+11)), 2);
-        mnemonic += words[value] + " ";
-    }
-    console.log(mnemonic);
-})
+    hash_binary = "0000".substr(hash_binary.length) + hash_binary;
+    // get only a slice of the hash as checksum
+    hash_binary = hash_binary.slice(0, (size / 32));
+    let checksummed_entropy_binary = entropy_binary + hash_binary;
+    return mapWords(checksummed_entropy_binary);
+}
+
+
+let mnemonic = gen_mnemonic(size)
+console.log(mnemonic)
+mapNumbers(mnemonic) 
